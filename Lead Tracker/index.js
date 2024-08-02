d7dde63da8598ed110d12e428952e0438d5f23e3
Raghavendra Shreevatsa SR <raghavv2710@gmail.com>
2024-08-02 22:25:06 +0530

    Update index.js

--- Lead Tracker/index.js
+++ Lead Tracker/index.js
@@ -1,10 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
 const firebaseConfig = {
-
+    databaseURL:'https://leads-tracker-app-b1684-default-rtdb.asia-southeast1.firebasedatabase.app/'
 }
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app)
+
+console.log(database)
 
 let myLeads = []
 const inputEl = document.getElementById("input-el")
